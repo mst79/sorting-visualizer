d85072bd586e597733ac0f7e817ef6cf8c5382c6
Manoj Singh Tomer <manojtomer0709@gmail.com>
2022-11-23 13:18:48 +0530

    heap-sort-added

--- script.js
+++ script.js
@@ -162,8 +162,8 @@ async function insertion_sort(array) {
       j = j - 1;
     }
 
-    bars[j + 1].style.backgroundColor = "blue"
-    await sleep_for(50);
+    bars[j + 1].style.backgroundColor = "grey"
+    await sleep_for(100);
     bars[j + 1].style.backgroundColor = "green"
     // array[j + 1] = key;
     bars[j + 1].style.height = array[j + 1] * height_factor + "px";
@@ -216,12 +216,77 @@ async function selection_sort(array){
 
       await sleep_for(speed_factor);
 
+      bars[i].style.backgroundColor = "black";
+      bars[jmin].style.backgroundColor = "black";
       bars[i].style.height = array[i] * height_factor + "px";
       bars[jmin].style.height = array[jmin] * height_factor + "px";
+
+      await sleep_for(300);
+
+      bars[i].style.backgroundColor = "floralwhite";
+      bars[jmin].style.backgroundColor = "floralwhite";
 
     }
 
     bars[i].style.backgroundColor = "green";
     await sleep_for(speed_factor);
   }
+}
+
+//-----------------------------HEAP SORT-----------------------------
+
+async function heap_sort(array) {
+  console.log(array);
+  let bars = document.getElementsByClassName("bar");
+  for (let i = Math.floor(array.length / 2); i >= 0; i--) {
+    await heapify(array, array.length, i);
+  }
+  for (let i = array.length - 1; i >= 0; i--) {
+    await swap(array, 0, i, bars);
+    await heapify(array, i, 0);
+
+  }
+
+  for (let k = 0; k < bars.length; k++) {
+    bars[k].style.backgroundColor = "aqua";
+    await sleep_for(speed_factor);
+  }
+  return array;
+}
+
+async function heapify(array, n, i) {
+  let bars = document.getElementsByClassName("bar");
+  let largest = i;
+  let left = 2 * i + 1;
+  let right = 2 * i + 2;
+  if (left < n && array[left] > array[largest]) {
+    largest = left;
+  }
+  if (right < n && array[right] > array[largest]) {
+    largest = right;
+  }
+  if (largest != i) {
+    await swap(array, i, largest, bars);
+    await heapify(array, n, largest);
+  }
+}
+
+async function swap(array, i, j, bars) {
+  let temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+  bars[i].style.height = array[i] * height_factor + "px";
+  bars[j].style.height = array[j] * height_factor + "px";
+  bars[i].style.backgroundColor = "red";
+  bars[j].style.backgroundColor = "red";
+  await sleep_for(speed_factor);
+
+  for (let k = 0; k < bars.length; k++) {
+    if (k != i && k != j) {
+      bars[k].style.backgroundColor = "aqua";
+    }
+  }
+  //bars[i].innerText = array[i];
+  //bars[j].innerText = array[j];
+  return array;
 }
